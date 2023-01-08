b5a0998b6f382b99235a913272aa682cab05f196
dharmraj-magdum <dkmagdum23@gmail.com>
2023-01-08 21:31:52 +0530

    all done set

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 console.log("script Running");
 
+var size = 100;
 const front = document.getElementById("front");
-const below = document.getElementById("below");
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
 
 document.onmousemove = function (e) {
 	// console.log("mouse location:", e.clientX, e.clientY);
-	let x = e.clientX;
-	let y = e.clientY;
+	let x = e.clientX - 90;
+	let y = e.clientY - 100;
 	front.style.setProperty("--x", `${x}px`);
 	front.style.setProperty("--y", `${y}px`);
+	front.style.setProperty("--size", `${size}px`);
 };
-setInterval(colorSwitch, 2000);
 
-function colorSwitch() {
-	front.classList.toggle("bg-black");
-	front.classList.toggle("bg-white");
-	below.classList.toggle("bg-white");
-	below.classList.toggle("bg-black");
+function increseHole() {
+	if (size < 300) {
+		size += 50;
+	}
+	// console.log(size);
+	if (size == 300) {
+		plus.disabled = true;
+		// console.log(plus.disabled);
+	}
+	minus.disabled = false;
+}
+function decreseHole() {
+	if (size >= 100) {
+		size -= 50;
+	}
+	// console.log(size);
+	if (size == 50) {
+		minus.disabled = true;
+		// console.log(plus.disabled);
+	}
+	plus.disabled = false;
 }
